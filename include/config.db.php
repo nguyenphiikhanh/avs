df45b44d3088a5e3dd8fc4987bec02a0468145de
mrK. Dev <mrKdev@gmail.com>
2024-01-22 17:47:00 +0700

    setting infra

--- include/config.db.php
+++ include/config.db.php
@@ -1,8 +1,8 @@
 <?php
 defined('_VALID') or die('Restricted Access!');
 $config['db_type'] = 'mysqli';
-$config['db_host'] = 'localhost';
-$config['db_user'] = '';
-$config['db_pass'] = '';
-$config['db_name'] = '';
+$config['db_host'] = 'mysql';
+$config['db_user'] = 'root';
+$config['db_pass'] = 'pass1234';
+$config['db_name'] = 'avscms';
 ?>
